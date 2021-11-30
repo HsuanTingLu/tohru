11cc92ce7161650f78feb76617f0ceb2f1506339
Hsuan-Ting Lu <hsuan.ting.lu.ee05@g2.nctu.edu.tw>
2021-11-30 08:05:24 +0800

    Add dependencies

--- setup.py
+++ setup.py
@@ -28,6 +28,9 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=[
         "numpy",
+        "pandas",
+        "dask",
+        "pyarrow",
     ],
     extras_require={
         "tests": [
